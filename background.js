0ff1b3c55fd6df0289fcc6ca56077b225228bc60
Rubén Martínez <rub3nmv@gmail.com>
2014-09-13 17:51:21 +0200

    Back to browser action + notification

--- background.js
+++ background.js
@@ -1,20 +1,7 @@
-// Called when the url of a tab changes.
-function checkForValidUrl(tabId, changeInfo, tab) {
-    // If the tabs url starts with "http://specificsite.com"...
-    var pattern = /^(http|https):\/\/www\.youtube\.com\/watch.*/g;
-    if (tab.url !== undefined && pattern.test(tab.url)) {
-        // ... show the page action.
-        chrome.pageAction.show(tabId);
-    }
-}
-
-// Listen for any changes to the URL of any tab.
-chrome.tabs.onUpdated.addListener(checkForValidUrl);
-
-chrome.pageAction.onClicked.addListener(function (tabId) {
+chrome.browserAction.onClicked.addListener(function (tabId) {
     var tabUrl = tabId.url;
-    // No need to check here, if the button exists the url is correct
-    if (tabUrl !== undefined) {
+    var pattern = /^(http|https):\/\/www\.youtube\.com\/watch.*/g;
+    if (tabUrl !== undefined && pattern.test(tabUrl)) {
         // Youtube video url
         window.open('http://savefrom.net/?url=' + encodeURIComponent(tabUrl));
     } else {
